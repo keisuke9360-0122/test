c63850b29cd9d1055d69592fe3b6c6a0ae92df14
keisuke9360 <keitsuru0122@gmail.com>
2023-08-14 22:43:02 +0900

    slick 横並び

--- js/index.js
+++ js/index.js
@@ -1,11 +1,23 @@
-console.log("感");
+// console.log("感");
+// $(function () {
+//   $("button").on("click", function () {
+//     console.log("呼び出し");
+//   });
+// });
+// $(".slide-items").slick({
+//   autoplay: true, // 自動再生
+//   arrows: true, // 矢印
+//   dots: true,
+// });
 $(function () {
-  $("button").on("click", function () {
-    console.log("呼び出し");
+  $(".slider_thumb").slick({
+    arrows: false,
+    asNavFor: ".thumb",
   });
-});
-$(".slide-items").slick({
-  autoplay: true, // 自動再生
-  arrows: true, // 矢印
-  dots: true,
+  $(".thumb").slick({
+    asNavFor: ".slider_thumb",
+    focusOnSelect: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+  });
 });
